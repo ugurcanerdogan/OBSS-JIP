496d8cb55f351de4b4c84d3419fb88e0f8508d8f
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-02 16:40:36 +0300

    DB error exception added

--- Spring/spring-demo/src/main/java/tr/com/obss/ji/springdemo/exception/GlobalExceptionHandler.java
+++ Spring/spring-demo/src/main/java/tr/com/obss/ji/springdemo/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -40,6 +41,15 @@ public class GlobalExceptionHandler {
 		LOGGER.error(t.getMessage(), t);
 		var map = new HashMap<>();
 		map.put("Error", t.getMessage());
+		return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<?> handleDatabaseException(HttpServletRequest request, Throwable t) {
+		LOGGER.error(t.getMessage(), t);
+		var map = new HashMap<>();
+		map.put("Error message", "DB error occured...");
 		return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
