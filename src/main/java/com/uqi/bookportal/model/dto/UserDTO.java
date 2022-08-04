19644930aaec98610ad96c77eac94449c956a950
Uğurcan Erdoğan <ugurcanerdogan3306@gmail.com>
2022-08-04 10:20:41 +0300

    model classes initalized

---
+++ src/main/java/com/uqi/bookportal/model/dto/UserDTO.java
@@ -0,0 +1,34 @@
+package com.uqi.bookportal.model.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserDTO {
+
+    @NotBlank
+    @Size(max = 255, min = 3, message = "Please enter a valid username")
+    @Email
+    private String username;
+
+    @NotBlank
+    @Size(max = 255, min = 3, message = "Please enter a valid password")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
